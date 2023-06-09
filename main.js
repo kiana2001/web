1a90b11bcc3c89fe73707391bbe7a67be1cafd5e
kiana <85028150+kiana2001@users.noreply.github.com>
2023-06-09 18:33:09 +0330

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('login-container');
+const popup = getElementById("btncv2")
+
+
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -9,3 +12,14 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+popup.addEventListener('click', () => {
+                          const box = document.getElementsByClassName('pop-up');
+
+                          box.style.display = 'none';
+                          });                          
+                          
+
+                          
+                        
+                        
