bc42f57d836a99351038f5e956fbc6995833212a
kiana <85028150+kiana2001@users.noreply.github.com>
2023-06-10 14:06:39 +0330

    adding a separate page for login

---
+++ web/main.js
@@ -0,0 +1,25 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('login-container');
+const popup = getElementById("btncv2")
+
+
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+popup.addEventListener('click', () => {
+                          const box = document.getElementsByClassName('pop-up');
+
+                          box.style.display = 'none';
+                          });                          
+                          
+
+                          
+                        
+                        
