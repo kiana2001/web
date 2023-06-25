d5b25b56a1f530d8dda731560a14c616f0b2563e
kiana2001 <kianakermani1381@gmail.com>
2023-06-25 13:42:57 +0330

    added some stuf

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('login-container');
+const popup = getElementById("btncv2")
+const sampleData = [
+    {
+        id:1,
+        name:"tehran"
+    },
+    {
+        id:2,
+        name:"mashhad"
+    }
+];
+
+
+
+const addHotelTickets = () => {
+    sampleData.map((item) => {
+        
+    })
+}
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -9,3 +28,14 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+popup.addEventListener('click', () => {
+                          const box = document.getElementsByClassName('pop-up');
+
+                          box.style.display = 'none';
+                          });                          
+                          
+
+                          
+    
+                        
