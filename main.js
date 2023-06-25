bee1709aa9ce1f3eea6fe09383fa5b7f795d694e
kiana2001 <kianakermani1381@gmail.com>
2023-06-25 15:29:18 +0330

    added hotel books

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
+
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('login-container');
 const popup = getElementById("btncv2")
-const sampleData = [
-    {
-        id:1,
-        name:"tehran"
-    },
-    {
-        id:2,
-        name:"mashhad"
-    }
-];
 
 
-
-const addHotelTickets = () => {
-    sampleData.map((item) => {
-        
-    })
-}
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
