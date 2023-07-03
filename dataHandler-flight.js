2a5cea81da37fec9da7c79661175286d9fb1430b
kiana2001 <kianakermani1381@gmail.com>
2023-07-03 12:06:35 +0330

    flight and hotl completion

--- dataHandler-flight.js
+++ dataHandler-flight.js
@@ -1,3 +1,197 @@
+// const hotelContainer = document.getElementById('hotelTicketsContainer');
+// const sampleData = [
+//     {
+//         id:1,
+//         name:"tehran",
+//         description: "xxxxx",
+//         location: "tehran",
+//         locationdestenation: "mashhad",
+//         imageSrc: "./images/pexels-pascal-borener-113017.jpg",
+//         numOf: 20,
+//         // date: 
+//         // datereverse:
+//         startDate: new Date("2023-06-28"),
+//         finishDate: new Date("2023-07-05"),
+//         togo: "./forms/page2.html"
+
+
+//     },
+//     {
+//         id:2,
+//         name:"mashhad",
+//         description: "yyyyyyy",
+//         location: "mashhad",
+//         imageSrc: "./images/pexels-pixabay-358220.jpg",
+//         locationdestenation: "tehran",
+//         numOf: 10,
+//         startDate: new Date("2023-06-28"),
+//         finishDate: new Date("2023-07-05"),
+//         togo: "./forms/page2.html"
+
+//     },
+//     {
+//         id:3,
+//         name:"shiraz",
+//         description: "yyyyyyy",
+//         location: "shiraz",
+//         imageSrc: "./images/plane222.jpg",
+//         locationdestenation: "shiraz",
+//         numOf: 30,
+//         startDate: new Date("2023-06-28"),
+//         finishDate: new Date("2023-07-05"),
+//         togo: "./forms/page2.html"
+
+//     },
+//     {
+//         id:4,
+//         name:"tabriz",
+//         description: "yyyyyyy",
+//         location: "tabriz",
+//         imageSrc: "./images/hotel4.jpg",
+//         locationdestenation: "mashhad",
+//                 numOf: 20,
+//                 startDate: new Date("2023-06-28"),
+//                 finishDate: new Date("2023-07-05"),
+//                 togo: "./forms/page2.html"
+
+//     },
+//     {
+//         id:5,
+//         name:"sari",
+//         description: "yyyyyyy",
+//         location: "sari",
+//         imageSrc: "./images/hotel3.jpg",
+//         locationdestenation: "mashhad",
+//                 numOf: 20,
+//                 startDate: new Date("2023-06-28"),
+//                 finishDate: new Date("2023-07-05"),
+//                 togo: "./forms/page2.html"
+
+//     },
+//     {
+//         id:5,
+//         name:"esfahan",
+//         description: "yyyyyyy",
+//         location: "esfahan",
+//         imageSrc: "./images/hotel3.jpg",
+//         locationdestenation: "mashhad",
+//          numOf: 20,
+//          startDate: new Date("2023-06-28"),
+//          finishDate: new Date("2023-07-05"),
+//     }
+// ];
+
+
+// let dataToShow;
+// const handleChange = (event) => {
+//   const { name, value } = event.target;
+
+//   if (name === 'location') {
+//     dataToShow = sampleData.filter(data => data.location.toLowerCase().includes(value.toLowerCase()));
+//   } else if (name === 'locationdestenation') {
+//     dataToShow = sampleData.filter(data => data.locationdestenation.toLowerCase().includes(value.toLowerCase()));
+//   } else if (name === 'numOf') {
+//     dataToShow = sampleData.filter(data => data.numOf >= parseInt(value));
+//   } else if (name === 'startDate' || name === 'finishDate') {
+//     const startDate = new Date(document.getElementById('startDate').value);
+//     const finishDate = new Date(document.getElementById('finishDate').value);
+
+//     dataToShow = sampleData.filter(data => {
+//       const dataDate = new Date(data.startDate);
+//       return dataDate >= startDate && dataDate <= finishDate;
+//     });
+//   }
+
+//   showData();
+// };
+
+
+  
+// // function filterDataByLocation(data, location, locationDestination) {
+// //   return data.filter(
+// //     item =>
+// //       item.location.toLowerCase() === location.toLowerCase() &&
+// //       item.locationdestenation.toLowerCase() === locationDestination.toLowerCase()
+// //   );
+// // }
+// // function filterByNumOfPeople(data, num) {
+// //     return data.filter(item => item.numOf === num);
+// //   }
+  
+// //   const filteredData = filterByNumOfPeople(sampleData, 2);
+// //   console.log(filteredData);
+
+// function filterByNumOfPeople(data, condition) {
+//     if (condition === 'below') {
+//       return data.filter(item => item.numOf < 20);
+//     } else if (condition === 'more') {
+//       return data.filter(item => item.numOf >= 20);
+//     } else {
+//       return data; // Return the original array if the condition is invalid
+//     }
+//   }
+  
+//   const filteredDataBelow20 = filterByNumOfPeople(sampleData, 'below');
+//   console.log('Below 20:', filteredDataBelow20);
+  
+//   const filteredDataMoreThan20 = filterByNumOfPeople(sampleData, 'more');
+//   console.log('More than 20:', filteredDataMoreThan20);
+
+
+//   const showData = () => {
+//     if(!dataToShow){
+//         dataToShow = sampleData;
+//     }
+//     while (hotelContainer.firstChild) {
+//         hotelContainer.removeChild(hotelContainer.firstChild);
+//     }
+//     dataToShow.map(data => {
+//         // Create the required HTML elements
+//         const colDiv = document.createElement('div');
+//         colDiv.classList.add('col');
+      
+//         const cardDiv = document.createElement('div');
+//         cardDiv.classList.add('card222');
+      
+//         const img = document.createElement('img');
+//         img.src = data.imageSrc;
+//         img.classList.add('card-img-top');
+//         img.alt = '...';
+      
+//         const cardBodyDiv = document.createElement('div');
+//         cardBodyDiv.classList.add('card222-body');
+      
+//         const titleH5 = document.createElement('h5');
+//         titleH5.classList.add('card-title');
+//         titleH5.textContent = data.name;
+      
+//         const descriptionP = document.createElement('p');
+//         descriptionP.classList.add('card-text');
+//         descriptionP.textContent = data.description;  
+//         const button = document.createElement('button');
+//         const buttonLink = document.createElement('a');
+//         buttonLink.href = './forms/index.html';
+//         buttonLink.classList.add('btn', 'btn-primary');
+//         buttonLink.textContent = 'more information';
+//         button.appendChild(buttonLink);
+      
+//         // Append the elements to build the hierarchy
+//         cardBodyDiv.appendChild(titleH5);
+//         cardBodyDiv.appendChild(descriptionP);
+//         cardBodyDiv.appendChild(button);
+      
+//         cardDiv.appendChild(img);
+//         cardDiv.appendChild(cardBodyDiv);
+      
+//         colDiv.appendChild(cardDiv);
+      
+//         hotelContainer.appendChild(colDiv);
+//       });
+//   }
+
+//   showData();
+
+
 const hotelContainer = document.getElementById('hotelTicketsContainer');
 const sampleData = [
     {
@@ -12,7 +206,7 @@ const sampleData = [
         // datereverse:
         startDate: new Date("2023-06-28"),
         finishDate: new Date("2023-07-05"),
-
+        togo: "./forms/index.html"
     },
     {
         id:2,
@@ -24,6 +218,7 @@ const sampleData = [
         numOf: 10,
         startDate: new Date("2023-06-28"),
         finishDate: new Date("2023-07-05"),
+        togo: "./forms/flight1.html"
     },
     {
         id:3,
@@ -35,6 +230,7 @@ const sampleData = [
         numOf: 30,
         startDate: new Date("2023-06-28"),
         finishDate: new Date("2023-07-05"),
+        togo: "./forms/flight2.html"
     },
     {
         id:4,
@@ -43,9 +239,11 @@ const sampleData = [
         location: "tabriz",
         imageSrc: "./images/hotel4.jpg",
         locationdestenation: "mashhad",
-                numOf: 20,
-                startDate: new Date("2023-06-28"),
-                finishDate: new Date("2023-07-05"),
+        numOf: 20,
+        startDate: new Date("2023-06-28"),
+        finishDate: new Date("2023-07-05"),
+        togo: "./forms/flight1.html"
+
     },
     {
         id:5,
@@ -54,9 +252,10 @@ const sampleData = [
         location: "sari",
         imageSrc: "./images/hotel3.jpg",
         locationdestenation: "mashhad",
-                numOf: 20,
-                startDate: new Date("2023-06-28"),
-                finishDate: new Date("2023-07-05"),
+        numOf: 20,
+        startDate: new Date("2023-06-28"),
+        finishDate: new Date("2023-07-05"),
+        togo: "./forms/flight1.html"
     },
     {
         id:5,
@@ -65,14 +264,15 @@ const sampleData = [
         location: "esfahan",
         imageSrc: "./images/hotel3.jpg",
         locationdestenation: "mashhad",
-         numOf: 20,
-         startDate: new Date("2023-06-28"),
-         finishDate: new Date("2023-07-05"),
+        numOf: 20,
+        startDate: new Date("2023-06-28"),
+        finishDate: new Date("2023-07-05"),
+        togo: "./forms/flight1.html"
     }
 ];
 
-
 let dataToShow;
+
 const handleChange = (event) => {
   const { name, value } = event.target;
 
@@ -95,22 +295,6 @@ const handleChange = (event) => {
   showData();
 };
 
-
-  
-// function filterDataByLocation(data, location, locationDestination) {
-//   return data.filter(
-//     item =>
-//       item.location.toLowerCase() === location.toLowerCase() &&
-//       item.locationdestenation.toLowerCase() === locationDestination.toLowerCase()
-//   );
-// }
-// function filterByNumOfPeople(data, num) {
-//     return data.filter(item => item.numOf === num);
-//   }
-  
-//   const filteredData = filterByNumOfPeople(sampleData, 2);
-//   console.log(filteredData);
-
 function filterByNumOfPeople(data, condition) {
     if (condition === 'below') {
       return data.filter(item => item.numOf < 20);
@@ -119,17 +303,16 @@ function filterByNumOfPeople(data, condition) {
     } else {
       return data; // Return the original array if the condition is invalid
     }
-  }
-  
-  const filteredDataBelow20 = filterByNumOfPeople(sampleData, 'below');
-  console.log('Below 20:', filteredDataBelow20);
-  
-  const filteredDataMoreThan20 = filterByNumOfPeople(sampleData, 'more');
-  console.log('More than 20:', filteredDataMoreThan20);
+}
 
+const filteredDataBelow20 = filterByNumOfPeople(sampleData, 'below');
+console.log('Below 20:', filteredDataBelow20);
 
-  const showData = () => {
-    if(!dataToShow){
+const filteredDataMoreThan20 = filterByNumOfPeople(sampleData, 'more');
+console.log('More than 20:', filteredDataMoreThan20);
+
+const showData = () => {
+    if (!dataToShow) {
         dataToShow = sampleData;
     }
     while (hotelContainer.firstChild) {
@@ -158,9 +341,10 @@ function filterByNumOfPeople(data, condition) {
         const descriptionP = document.createElement('p');
         descriptionP.classList.add('card-text');
         descriptionP.textContent = data.description;  
+      
         const button = document.createElement('button');
         const buttonLink = document.createElement('a');
-        buttonLink.href = './forms/index.html';
+        buttonLink.href = data.togo;
         buttonLink.classList.add('btn', 'btn-primary');
         buttonLink.textContent = 'more information';
         button.appendChild(buttonLink);
@@ -176,7 +360,7 @@ function filterByNumOfPeople(data, condition) {
         colDiv.appendChild(cardDiv);
       
         hotelContainer.appendChild(colDiv);
-      });
-  }
+    });
+}
 
-  showData();
+showData();
